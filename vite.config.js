6591dd17bf6a92e73004661ced1b665fef49f40e
Folkdance2025 <folkdance2025@gmail.com>
2025-06-24 08:50:55 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',           // ✅ 根目錄部署，不需要加 repo 名稱
-  build: {
-    outDir: 'dist'     // ✅ 與 gh-pages 預設相同
-  }
+  plugins: [react()],
 })
