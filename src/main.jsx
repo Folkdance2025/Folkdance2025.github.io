fef5d18c60b0e5a6d99e193b4c7a0064cd41f280
Folkdance2025 <folkdance2025@gmail.com>
2025-07-15 11:15:25 +0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,11 @@
-// src/main.jsx
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import "./i18n"; // ← 加入這行
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-// 處理 GitHub Pages 的 404.html 導回原路徑
-const redirectPath = sessionStorage.getItem('redirect-path')
-if (redirectPath) {
-  sessionStorage.removeItem('redirect-path')
-  window.history.replaceState(null, '', redirectPath)
-}
-
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
-)
+);
